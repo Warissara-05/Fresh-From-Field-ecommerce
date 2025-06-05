268378f1824a638fa606abf666cdf4def1e89f5a
Warissara48 <xarmdiw@gmail.com>
2025-06-05 11:53:08 +0700

    login and register

---
+++ backend/apiServer.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 2000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/register', require('./routes/register.js'));
+app.use('/api/login', require('./routes/login.js'));
+
+app.listen(PORT, () => {
+    console.log("Server running at http://localhost:"+PORT);
+})
